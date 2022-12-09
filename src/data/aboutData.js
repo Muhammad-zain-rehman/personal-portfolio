515ec6dcbfa34b1edba85db15f56e560d181bdad
Muhammad Zain Rehman <83566399+Zain-Astute@users.noreply.github.com>
2022-12-10 00:02:38 +0500

    change self description:star:

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Zain. I'm a web designer and developer based in Lahore, Pakistan.",
+    description1: "I'm Zain a web designer and developer based in Lahore, Pakistan.",
     description2: "During the day I work as a React developer at a local agency and in the evening I work on freelance projects and utilize the time to get experience to built my own Products in Future. I spent my leisure hours writing articles. Right now I'm also trying a hand at Metaverse and Augmented Reality. I love to learn and explore new arenas.",
     image: 2
 }
